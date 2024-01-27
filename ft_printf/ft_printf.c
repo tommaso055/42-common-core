be05982673858c93dd46ca3bfe85d75401a0ca03
Tommaso Donato <tdonato@e4r2p1.42roma.it>
2024-01-27 17:23:33 +0100

    slfj

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,30 +1,63 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-int ft_put(char c, va_list args)
+int	ft_putchar_and_str(char c, char *str)
 {
-	if (c == 'c')
-		ft_putchar();
-	else if (c == 'c')
-		put();
-	else if (c == 's')
-		put();
-	else if (c == 'p')
-		put();
-	else if (c == 'd')
-		put();
-	else if (c == 'i')
-		put();
-	else if (c == 'u')
-		put();
-	else if (c == 'x')
-		put();
-	else if (c == 'X')
-		put();
-	else if (c == '%')
-		put();
-	else
-		return (1);
+	int	return_value;
+	int i;
+
+	i = 0;
+	if (c)
+		return_value += write(1, &c, 1);
+	if (str)
+	{
+		while (str[i])
+			return_value += write(1, &str[i++], 1);
+	}
+	return (return_value);
+}
+
+
+int	ft_putnbr_base(int n, int base, char *charset)
+{
+	int	return_value;
+
+	return_value = 0;
+	if (n < 0)
+	{
+		return_value += ft_putchar_and_str('-', NULL);
+		n *= -1;
+	}
+	if (n >= base)
+		return_value += ft_putnbr_base(n / base, base, charset);
+	ft_putchar_and_str(charset[n % base], NULL);
+	return (return_value + 1);
+}
+
+int ft_put(char flag, va_list *args)
+{
+	int	return_value;
+
+	return_value = 0;
+	if (flag == 'c')
+		return_value += ft_putchar_and_str((char)va_arg(*args, int), NULL);
+	else if (flag == 's')
+		return_value += ft_putchar_and_str(0, va_arg(*args, char *));
+	else if (flag == 'p')
+		return ;
+	else if (flag == 'd')
+		return_value += ft_putnbr_base(va_arg(*args, int), 10, "0123456789");
+	else if (flag == 'i')
+		return_value += ft_putnbr_base(va_arg(*args, int), 10, "0123456789");
+	else if (flag == 'u')
+		return ;
+	else if (flag == 'x')
+		return_value += ft_putnbr_base(va_arg(*args, int), 16, "0123456789abcdef");
+	else if (flag == 'X')
+		return_value += ft_putnbr_base(va_arg(*args, int), 16, "0123456789ABCDEF");
+	else if (flag == '%')
+		return_value += ft_putchar_and_str('%', NULL);
+	return (return_value);
 }
 
 int ft_printf(const char *format, ...)
@@ -39,12 +72,12 @@ int ft_printf(const char *format, ...)
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
-		{
-			i++;
-			printed_chars += ft_put(format[i], args);
-		}
+			printed_chars += ft_put(format[++i], &args);
 		else
+		{
 			write(1, &format[i], 1);
+			printed_chars++;
+		}
 		i++;
 	}
 	return (printed_chars);
