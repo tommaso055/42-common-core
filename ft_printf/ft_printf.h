5b37964274507d3a1c4014efc28503b001a59ba7
tommaso055 <71332685+tommaso055@users.noreply.github.com>
2024-01-31 20:19:28 +0100

    Update ft_printf.h

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdonato <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 20:02:48 by tdonato           #+#    #+#             */
+/*   Updated: 2024/01/31 20:02:56 by tdonato          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
@@ -5,6 +17,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int ft_printf(const char *format, ...);
+int	ft_printf(const char *format, ...);
 
 #endif
