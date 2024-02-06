aaa8a77fa16ee6b557b42b752f738ee3c38282b7
tommaso055 <71332685+tommaso055@users.noreply.github.com>
2024-02-06 16:51:54 +0100

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,12 +1,14 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFFER_SIZE 10
 
 # include <stdlib.h>
 # include <unistd.h>
 
-char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
 
 #endif
