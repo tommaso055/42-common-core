6fd52d373ef794b65fafb3d710fd2470d71948a7
tommaso055 <71332685+tommaso055@users.noreply.github.com>
2024-02-06 16:50:49 +0100

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,89 +1,32 @@
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *str)
-{
-	size_t	len;
-
-	len = 0;
-	while (*str)
-	{
-		str++;
-		len++;
-	}
-	return (len);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	int		i;
-	int		j;
-	char	*ans;
-	int		space_to_allocate;
-
-	if (!s1 || !s2)
-		return (NULL);
-	i = -1;
-	j = -1;
-	space_to_allocate = ft_strlen(s1) + ft_strlen(s2) + 1;
-	ans = (char *)malloc(space_to_allocate * sizeof(char));
-	if (ans == NULL)
-		return (NULL);
-	while (s1[++i])
-		ans[i] = s1[i];
-	while (s2[++j])
-		ans[i + j] = s2[j];
-	ans[i + j] = '\0';
-	return (ans);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*substring;
-	size_t	i;
-	size_t	s_len;
-
-	if (!s || len < 0)
-		return (NULL);
-	i = 0;
-	s_len = ft_strlen(s);
-	if (s_len < len)
-		len = s_len;
-	substring = (char *)malloc((len + 1) * sizeof(char));
-	if (substring == NULL)
-		return (NULL);
-	while (i < len && start <= s_len)
-	{
-		substring[i] = s[i + start];
-		i++;
-	}
-	substring[i] = '\0';
-	return (substring);
-}
-
 char	*get_next_line(int fd)
 {
 	static char	*remaining;
 	size_t		i;
 	char		*temp_str;
 	int			keep_itering;
-	char		*c;
+	char		*buffer;
 	char		*return_str;
 
 	i = 0;
-	c = (char *)malloc((BUFF_SIZE + 1) * sizeof(char));
-	if (!c)
-		return (NULL);
 	keep_itering = 1;
 	remaining = NULL;
+	buffer = malloc(BUFFER_SIZE + 1);
 	if (remaining)
 	{
-		temp_str = (char *)malloc((BUFF_SIZE + 1) * sizeof(char));
+		temp_str = malloc(ft_strlen(remaining) + 1);
 		if (!temp_str)
 			return (NULL);
-		temp_str = remaining;
+		ft_strlcpy(temp_str, remaining, BUFFER_SIZE);
 	}
 	else
-		read(fd, temp_str, BUFF_SIZE);
+	{
+		temp_str = malloc(BUFFER_SIZE + 1);
+		if (!temp_str)
+			return (NULL);
+		read(fd, temp_str, BUFFER_SIZE);
+	}
 	while (keep_itering)
 	{
 		while (temp_str[i] != '\0' && keep_itering)
@@ -94,15 +37,14 @@ char	*get_next_line(int fd)
 		}
 		if (keep_itering)
 		{
-			read(fd, c, BUFF_SIZE);
-			if (!c)
+			read(fd, buffer, BUFFER_SIZE);
+			if (!buffer)
 				return (NULL);
-			temp_str = ft_strjoin(temp_str, c);
+			temp_str = ft_strjoin(temp_str, buffer);
 		}
 	}
 	remaining = ft_substr(temp_str, i, sizeof(temp_str) - i);
 	return_str = ft_substr(temp_str, 0, i);
-	// free(temp_str);
 	return (return_str);
 }
 
