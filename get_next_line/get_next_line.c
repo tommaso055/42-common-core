7def07a3a3c61afc025de2b81aedc63fe97c2229
Tommaso Donato <tdonato@Tommasos-MBP.homenet.telecomitalia.it>
2024-02-10 01:20:33 +0100

    bada

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -25,14 +25,15 @@ char	*get_next_line(int fd)
 		temp_str = malloc(BUFFER_SIZE + 1);
 		if (!temp_str)
 			return (NULL);
-		read(fd, temp_str, BUFFER_SIZE);
+		if (read(fd, temp_str, BUFFER_SIZE) == 0)
+			return (NULL);
 	}
 	while (keep_itering)
 	{
-		if (temp_str[i] == -1)
-			return (NULL);
 		while (temp_str[i] != '\0' && keep_itering)
 		{
+			if (temp_str[i] == -1)
+				return (temp_str);
 			if(temp_str[i] == '\n')
 				keep_itering = 0;
 			i++;
@@ -41,16 +42,12 @@ char	*get_next_line(int fd)
 		{
 			if (read(fd, buffer, BUFFER_SIZE) == 0)
 				return (NULL);
-			printf("#\n#\n#\n%s <-(buffer)\n", buffer);
-			printf("%s <-(temp_str)\n", temp_str);
 			temp_str = ft_strjoin(temp_str, buffer);
-			printf("%s <-(all)\n#\n#\n#\n", temp_str);
 		}
 	}
-	printf("%s <-(stringa da printare in parte\n#\n#\n)", return_str);
-	remaining = ft_substr(temp_str, i, sizeof(temp_str) - i);
+	remaining = ft_substr(temp_str, i, ft_strlen(temp_str + i));
 	return_str = ft_substr(temp_str, 0, i);
-	printf("%s <-(stringa printata)\n", return_str);
+	free(temp_str);
 	return (return_str);
 }
 
@@ -58,22 +55,24 @@ char	*get_next_line(int fd)
 int main()
 {
 	int fd;
+
 	fd = open("get_next_line.c", O_RDONLY);
+
 	if (fd == -1)
 	{
 		perror("Error opening file");
 		return 1;
 	}
+
 	char *line;
 
-	int i = 20;
-	while (i--)
-	// while ((line = get_next_line(fd)) != NULL)
+	while ((line = get_next_line(fd)) != NULL)
 	{
-		line = get_next_line(fd);
-		// printf("%s", line);
+		printf("%s", line);
 		free(line);
 	}
+
 	close(fd);
+
 	return 0;
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,14 +1,14 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 10
+# define BUFFER_SIZE 1
 
 # include <stdlib.h>
 # include <unistd.h>
 
 size_t	ft_strlen(const char *str);
-size_t	ft_strlcpy(char *dst, char *src, size_t size);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_substr(char *s, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*get_next_line(int fd);
 
 #endif
