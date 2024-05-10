79314a396bd52b0f708e28812a4cb92589d66391
tommaso055 <71332685+tommaso055@users.noreply.github.com>
2024-05-10 21:57:25 +0200

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -14,6 +14,108 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+// size_t	ft_strlen(const char *str)
+// {
+// 	size_t	len;
+
+// 	len = 0;
+// 	while (str && *str != 0)
+// 	{
+// 		str++;
+// 		len++;
+// 	}
+// 	return (len);
+// }
+
+// int		contains(char *str, char c)
+// {
+// 	int	i;
+
+// 	if (!str)
+// 		return (0);
+// 	i = 0;
+// 	while (str[i])
+// 	{
+// 		if (str[i++] == c)
+// 			return (1);
+// 	}
+// 	return (0);
+// }
+
+// size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	if (size > 0)
+// 	{
+// 		while (i < size - 1 && src[i])
+// 		{
+// 			dst[i] = src[i];
+// 			i++;
+// 		}
+// 		dst[i] = '\0';
+// 	}
+// 	while (src[i])
+// 		i++;
+// 	return (i);
+// }
+
+// char	*ft_strjoin(char *back, char *buff)
+// {
+// 	char	*str;
+// 	size_t	i;
+// 	size_t	j;
+
+// 	if (!back)
+// 	{
+// 		back = malloc(1 * sizeof(char));
+// 		back[0] = '\0';
+// 	}
+// 	if (!back || !buff)
+// 		return (NULL);
+// 	str = malloc(sizeof(char) * ((ft_strlen(back) + ft_strlen(buff)) + 1));
+// 	if (str == NULL)
+// 		return (NULL);
+// 	i = -1;
+// 	j = 0;
+// 	if (back)
+// 		while (back[++i] != '\0')
+// 			str[i] = back[i];
+// 	while (buff[j] != '\0')
+// 		str[i++] = buff[j++];
+// 	str[ft_strlen(back) + ft_strlen(buff)] = '\0';
+// 	free (back);
+// 	return (str);
+// }
+
+// char	*ft_substr(char *s, unsigned int start, size_t len, int free_s)
+// {
+// 	char	*substring;
+// 	size_t	i;
+// 	size_t	s_len;
+
+// 	i = 0;
+// 	if (!s || !s[i] || len == 0)
+// 	{
+// 		if (free_s)
+// 			free(s);
+// 		return (NULL);
+// 	}
+// 	substring = (char *)malloc((len + 1) * sizeof(char));
+// 	if (substring == NULL)
+// 		return (NULL);
+// 	while (i < len && start <= ft_strlen(s))
+// 	{
+// 		substring[i] = s[i + start];
+// 		i++;
+// 	}
+// 	substring[i] = '\0';
+// 	if (free_s)
+// 		free(s);
+// 	return (substring);
+// }
+
 
 char	*readfile(int fd, char *static_memory)
 {
@@ -21,12 +123,17 @@ char	*readfile(int fd, char *static_memory)
 	char	*buffer;
 
 	bytes = 1;
-	while (!contains(static_memory, '\n'), bytes > 0)
+	while (!contains(static_memory, '\n') && bytes > 0)
 	{
 		buffer = malloc(BUFFER_SIZE + 1);
 		bytes = read(fd, buffer, BUFFER_SIZE);
-		buffer[bytes] = 0;
-		static_memory = ft_strjoin(static_memory, buffer);
+		if (!buffer)
+			return (NULL);
+		if (bytes > 0)
+		{
+			buffer[bytes] = 0;
+			static_memory = ft_strjoin(static_memory, buffer);
+		}
 		free(buffer);
 	}
 	return (static_memory);
@@ -39,11 +146,12 @@ ssize_t	get_nl_index(char *static_memory)
 	index = 0;
 	while (static_memory[index] != 0)
 	{
-		if (static_memory[index++] == 0)
+		if (static_memory[index++] == '\n')
 			return (index);
 	}
-	return (-1);
+	return (index);
 }
+
 
 char	*get_next_line(int fd)
 {
@@ -56,128 +164,14 @@ char	*get_next_line(int fd)
 	static_memory = readfile(fd, static_memory);
 	if (!static_memory)
 		return (NULL);
-	if (!contains(static_memory, '\n'))
-	{
-		current_line = malloc(ft_strlen(static_memory) + 1);
-		ft_strlcpy(current_line, static_memory, ft_strlen(static_memory) + 1);
-		free(static_memory);
-		return (current_line);
-	}
 	index = get_nl_index(static_memory);
-	current_line = ft_substr(static_memory, 0, index);
-	static_memory = ft_substr(static_memory, index, ft_strlen(static_memory + index));
+	current_line = ft_substr(static_memory, 0, index, 0);
+	static_memory = ft_substr(static_memory, index, ft_strlen(static_memory + index), 1);
+	// current_line = ft_writing(static_memory);
+	// static_memory = ft_new_static(static_memory);
 	return(current_line);
 }
 
-// #include <fcntl.h>
-// #include <stdio.h>
-
-// int main() {
-// 	int fd = open("gnlTester-master/files/nl", O_RDONLY);
-// 	char *c = get_next_line(fd);
-// 	while(c != NULL)
-// 	{
-// 		printf("%s", c);
-// 		free(c);
-// 		c = get_next_line(fd);
-// 	}
-// 	free(c);
-// 	return 0;
-// }
-
-
-char	*initialize_from(int fd, char *remaining)
-{
-	int		space_to_allocate;
-	char	*return_str;
-
-	space_to_allocate = BUFFER_SIZE + 1;
-	if (ft_strlen(remaining))
-		space_to_allocate = ft_strlen(remaining) + 1;
-
-	return_str = malloc(space_to_allocate);
-	if (!return_str)
-		return (NULL);
-
-	if (ft_strlen(remaining))
-		ft_strlcpy(return_str, remaining, ft_strlen(remaining) + 1);
-	else
-	{
-		if (read(fd, return_str, BUFFER_SIZE) <= 0)
-			{
-				free(return_str);
-				return_str = NULL;
-			}
-	}
-	return (return_str);
-}
-
-void	until_nl(int fd, char **next_line)
-{
-	size_t		i;
-	int			keep_itering;
-	char		*buffer;
-	int			bytes;
-
-	i = 0;
-	keep_itering = 1;
-	while (keep_itering)
-	{
-		while (next_line[0][i] != 0 && keep_itering)
-			if (next_line[0][i++] == '\n')
-				keep_itering = 0;
-		if (keep_itering)
-		{
-			buffer = malloc(BUFFER_SIZE + 1);
-			bytes = read(fd, buffer, BUFFER_SIZE);
-			if (bytes <= 0)
-			{
-				free(buffer);
-				return ;
-			}
-			buffer[bytes] = '\0';
-			*next_line = ft_strjoin(*next_line, buffer);
-		}
-	}
-}
-
-ssize_t	get_nl_index(char *next_line)
-{
-	ssize_t	index;
-
-	index = 0;
-	while(next_line[index] != 0)
-	{
-		if (next_line[index++] == '\n')
-			return (index);
-	}
-	return (-1);
-}
-
-char	*get_next_line(int fd)
-{
-	static char	*remaining;
-	char		*next_line;
-	ssize_t		index;
-	char		*return_str;
-
-	next_line = initialize_from(fd, (char *)remaining);
-	free(remaining);
-	if (!next_line)
-		return (NULL);
-	
-	until_nl(fd, &next_line);
-	index = get_nl_index(next_line);
-	if (index == -1)
-	{
-		remaining = NULL;
-		return (next_line);
-	}
-	remaining = ft_substr(next_line, index, ft_strlen(next_line + index));
-	return_str = ft_substr(next_line, 0, index);
-	free(next_line);
-	return (return_str);
-}
 
 // #include <fcntl.h>
 // #include <stdio.h>
