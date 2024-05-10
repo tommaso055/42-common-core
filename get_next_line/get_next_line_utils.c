0a5e29aa78fca655afd79cfd644688170eeadcc0
tommaso055 <71332685+tommaso055@users.noreply.github.com>
2024-05-10 22:16:44 +0200

    Update get_next_line_utils.c

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -25,7 +25,7 @@ size_t	ft_strlen(const char *str)
 	return (len);
 }
 
-int		contains(char *str, char c)
+int	contains(char *str, char c)
 {
 	int	i;
 
@@ -38,25 +38,6 @@ int		contains(char *str, char c)
 			return (1);
 	}
 	return (0);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	size_t	i;
-
-	i = 0;
-	if (size > 0)
-	{
-		while (i < size - 1 && src[i])
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	while (src[i])
-		i++;
-	return (i);
 }
 
 char	*ft_strjoin(char *back, char *buff)
