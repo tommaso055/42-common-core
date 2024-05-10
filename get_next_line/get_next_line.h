0c45470d293671a6b2041960c96d01ea6a1e3a22
tommaso055 <71332685+tommaso055@users.noreply.github.com>
2024-05-10 21:56:53 +0200

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -24,7 +24,7 @@ size_t	ft_strlen(const char *str);
 int		contains(char *str, char c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_substr(char *s, unsigned int start, size_t len, int free_s);
 char	*get_next_line(int fd);
 
 #endif
